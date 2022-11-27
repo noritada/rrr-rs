8ec06f471ecb3ffd290dc3d0bb44cf8179d79415
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-11-28 02:32:53 +0900

    cli: add a subcommand "completions" to generate shell completions

--- cli/src/command/mod.rs
+++ cli/src/command/mod.rs
@@ -2,11 +2,17 @@ use anyhow::Result;
 use clap::{ArgMatches, Command};
 
 pub(crate) fn cli() -> Vec<Command> {
-    vec![dump::cli(), header::cli(), schema::cli()]
+    vec![
+        completions::cli(),
+        dump::cli(),
+        header::cli(),
+        schema::cli(),
+    ]
 }
 
 pub(crate) async fn dispatch(matches: ArgMatches) -> Result<()> {
     match matches.subcommand() {
+        Some(("completions", args)) => completions::exec(args).await?,
         Some(("dump", args)) => dump::exec(args).await?,
         Some(("header", args)) => header::exec(args).await?,
         Some(("schema", args)) => schema::exec(args).await?,
@@ -15,6 +21,7 @@ pub(crate) async fn dispatch(matches: ArgMatches) -> Result<()> {
     std::process::exit(0)
 }
 
+mod completions;
 mod dump;
 mod header;
 mod schema;
