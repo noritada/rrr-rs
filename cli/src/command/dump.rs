a79552c8f1020c834d82a8ed633eb402a956abae
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-12-31 00:10:11 +0900

    cli: add a new `--ignore-size` option to the `dump` command

--- cli/src/command/dump.rs
+++ cli/src/command/dump.rs
@@ -1,17 +1,26 @@
 use crate::common::read_from_source;
 use anyhow::Result;
-use clap::{arg, ArgMatches, Command};
+use clap::{arg, ArgAction, ArgMatches, Command};
 use rrr::{DataReaderOptions, JsonDisplay};
 
 pub(crate) fn cli() -> Command {
     Command::new("dump")
         .about("Dump the data of the specified file")
+        .arg(
+            arg!(--"ignore-size" r#"Ignore the value of "data_size" field in reading"#)
+                .action(ArgAction::SetTrue),
+        )
         .arg(arg!(<PATH_OR_URI> "Path or S3 URI of the file").required(true))
 }
 
 pub(crate) async fn exec(args: &ArgMatches) -> Result<()> {
     let fname = args.get_one::<String>("PATH_OR_URI").unwrap();
     let options = DataReaderOptions::ENABLE_READING_BODY;
+    let options = if args.get_flag("ignore-size") {
+        options.union(DataReaderOptions::IGNORE_DATA_SIZE_FIELD)
+    } else {
+        options
+    };
     let (schema, _, body_buf) = read_from_source(fname, None, options).await?;
 
     println!("{}", JsonDisplay::new(&schema, &body_buf));
