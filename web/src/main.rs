be74585a6342fc0f55e78f235406f978683cdc1d
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-06-25 19:47:43 +0900

    web: display the schema tree

--- web/src/main.rs
+++ web/src/main.rs
@@ -2,6 +2,7 @@ use std::ops::Deref;
 
 use drop_area::FileDropArea;
 use gloo_file::{futures::read_as_bytes, Blob};
+use rrr::AstVisitor;
 use yew::prelude::*;
 
 mod drop_area;
@@ -10,7 +11,9 @@ mod tree;
 #[function_component(App)]
 fn app() -> Html {
     let dropped_file = use_state(|| None);
+    let file_content = use_state(|| None);
     let body_json = use_state(|| None);
+    let schema_tree = use_state(|| None);
 
     let on_file_drop = {
         let dropped_file = dropped_file.clone();
@@ -24,7 +27,7 @@ fn app() -> Html {
     };
 
     {
-        let body_json = body_json.clone();
+        let file_content = file_content.clone();
         let file = dropped_file.clone();
         use_effect_with_deps(
             move |_| {
@@ -37,15 +40,8 @@ fn app() -> Html {
                                 std::io::Cursor::new(&bytes),
                                 rrr::DataReaderOptions::ENABLE_READING_BODY,
                             );
-                            let json = reader.read().map(|(schema, _, body_buf)| {
-                                rrr::JsonDisplay::new(
-                                    &schema,
-                                    &body_buf,
-                                    rrr::JsonFormattingStyle::Pretty,
-                                )
-                                .to_string()
-                            });
-                            body_json.set(json.ok())
+                            let triplet = reader.read();
+                            file_content.set(triplet.ok())
                         }
                     });
                 }
@@ -53,6 +49,43 @@ fn app() -> Html {
             dropped_file,
         );
     }
+
+    {
+        let schema_tree = schema_tree.clone();
+        let triplet = file_content.clone();
+        let file_content = file_content.clone();
+        use_effect_with_deps(
+            move |_| {
+                if let Some((schema, _, _)) = triplet.as_ref() {
+                    let mut formatter = tree::SchemaTreeFormatter;
+                    schema_tree.set(formatter.visit(&schema.ast).ok());
+                }
+            },
+            file_content,
+        );
+    }
+
+    {
+        let body_json = body_json.clone();
+        let triplet = file_content.clone();
+        use_effect_with_deps(
+            move |_| {
+                if let Some((schema, _, body_buf)) = triplet.as_ref() {
+                    let json =
+                        rrr::JsonDisplay::new(&schema, &body_buf, rrr::JsonFormattingStyle::Pretty)
+                            .to_string();
+                    body_json.set(Some(json))
+                }
+            },
+            file_content,
+        );
+    }
+
+    let schema_tree_view = if let Some(schema_tree) = schema_tree.as_ref() {
+        schema_tree.clone()
+    } else {
+        html! {}
+    };
 
     let body_json = if let Some(json) = body_json.as_ref() {
         json.to_string()
@@ -69,6 +102,7 @@ fn app() -> Html {
                     <div>{ file_name }</div>
                 </div>
             </div>
+            <div id="schema-pane" class="pane tree"><div>{ schema_tree_view }</div></div>
             <div id="view-pane" class="pane">
                 <div>{ body_json }</div>
             </div>
